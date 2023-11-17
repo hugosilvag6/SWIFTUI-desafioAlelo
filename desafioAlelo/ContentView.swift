3546412dab28920b06bc5757fa8df6e1768dd2ad
Hugo Silva <hugosilvag6@hotmail.com>
2023-11-17 12:34:57 -0500

    first

---
+++ desafioAlelo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  desafioAlelo
+//
+//  Created by Hugo Silva on 17/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ desafioAlelo/desafioAleloApp.swift
@@ -0,0 +1,17 @@
+//
+//  desafioAleloApp.swift
+//  desafioAlelo
+//
+//  Created by Hugo Silva on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct desafioAleloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
